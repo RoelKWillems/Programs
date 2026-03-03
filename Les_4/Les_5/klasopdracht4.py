7bf990a940a91eb4669d914a747a762091747434
Roel_Willems <roel.willems@student.howest.be>
2026-03-03 13:30:31 +0100

    Klasopdracht4

---
+++ Les_4/Les_5/klasopdracht4.py
@@ -0,0 +1,13 @@
+x = (0)
+y = (13)
+z = (100)
+a = (-7)
+b = (-21)
+
+
+print(f"{x} is positief")
+print(f"{y} is positief")
+print(f"{a} is negatief")
+print(f"{z} is positief")
+print(f"{b} is negatief")
+
